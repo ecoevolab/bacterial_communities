dec86cb55b23b02c9051ae39f8904fb622fac9dc
mayra-mendoza <mayra.mendozavelazquez@gmail.com>
2026-04-27 15:52:00 -0600

    plots

--- 02_Scripts/03_bayesplot_testing.R
+++ 02_Scripts/03_bayesplot_testing.R
@@ -4,6 +4,13 @@
 library (bayesplot)
 library(ggplot2)
 library(hexbin)
+library(dplyr)
+library(tibble)
+library(posterior)
+library(tidyr)
+library(stringr)
+library(rstan)
+library(gridExtra)
 
 # To do list: 
 # * bayesplot - pairs 
@@ -114,35 +121,155 @@ for (i in 1:length(stan_toutputs)){
 
 dev.off()
 
+# r values bxpl -----------------------------------------------------------------
 
-# r & k values -----------------------------------------------------------------
-
-lista_r <- lapply(names(stan_toutputs), function(nm) {
+list_r <- lapply(names(stan_toutputs), function(nm) {
   # 
   
   draws <- as_draws_df(stan_toutputs[[nm]]) %>%
     select(r) %>% # select the r value in each spp 
-    mutate(ID = nombre) # keep the name 
+    mutate(ID = nm) # keep the name 
   return(draws)
 })
 
 # Bind all the rows in the object and make two columns one for each variable 
-df_boxplot <- bind_rows(lista_r) %>% 
+df_boxplot <- bind_rows(list_r) %>% 
 separate(ID, into = c("Strain", "Temperature"), sep = "_T") 
 
 df_boxplot <- df_boxplot %>%
+   
+  # generate a new "sampling_times column 
   mutate(
-
-      # generate a new "sampling_times column 
       sampling_times = case_when(
       str_detect(Temperature, "_6t")  ~ 6, # for the CH29 special cases 
       str_detect(Temperature, "_10t") ~ 10,
       TRUE                           ~ 10
     ),
+    
     # to clean the temperature column 
     Temperature = str_replace_all(Temperature, "_6t|_10t", "")
   )
 
 df_boxplot$Temperature <- as.numeric(df_boxplot$Temperature)
+bxplot_rdf <- as.data.frame(df_boxplot)
+
+ggplot(bxplot_rdf, aes(y = r, 
+                x = Strain, 
+                fill = factor(Temperature))) +
+  geom_boxplot() +
+  theme_bw() +
+  facet_wrap(~ Strain, scale="free")+ 
+  scale_fill_manual(values = c("rosybrown1", "rosybrown3", "pink4"))+
+  scale_y_continuous(name = "r values") +
+  scale_x_discrete(name = "Strain") +
+  ggtitle("r values by temperature") +
+  theme(plot.title = element_text(hjust = 0.5))
+
+# k values bxpl -----------------------------------------------------------------
+
+list_k <- lapply(names(stan_toutputs), function(nm) {
+  # 
+  
+  drawsk <- as_draws_df(stan_toutputs[[nm]]) %>%
+    select(k) %>% # select the r value in each spp 
+    mutate(ID = nm) # keep the name 
+  return(drawsk)
+})
+
+# Bind all the rows in the object and make two columns one for each variable 
+df_boxplotk <- bind_rows(list_k) %>% 
+  separate(ID, into = c("Strain", "Temperature"), sep = "_T") 
+
+df_boxplotk <- df_boxplotk %>%
+  
+  # generate a new "sampling_times column 
+  mutate(
+    sampling_times = case_when(
+      str_detect(Temperature, "_6t")  ~ 6, # for the CH29 special cases 
+      str_detect(Temperature, "_10t") ~ 10,
+      TRUE                           ~ 10
+    ),
+    
+    # to clean the temperature column 
+    Temperature = str_replace_all(Temperature, "_6t|_10t", "")
+  )
+
+df_boxplotk$Temperature <- as.numeric(df_boxplotk$Temperature)
+bxplot_kdf <- as.data.frame(df_boxplotk)
+
+ggplot(bxplot_kdf, aes(y = k, 
+                       x = Strain, 
+                       fill = factor(Temperature))) +
+  geom_boxplot() +
+  theme_bw() +
+  facet_wrap(~ Strain, scale="free")+ 
+  scale_fill_manual(values = c("tomato3", "tomato4", "red4"))+
+  scale_y_continuous(name = "r values") +
+  scale_x_discrete(name = "Strain") +
+  ggtitle("r values by temperature") +
+  theme(plot.title = element_text(hjust = 0.5))
 
 
+# stan.hist ----------------------------------------------------------------------
+
+pdf("03_Output/bayesplot_rk_plots/Histogram_rk.pdf", width = 10, height = 5 , onefile = TRUE)
+
+for (i in 1:length(stan_toutputs)){
+  # Strain for title 
+  strain <- names(stan_toutputs)[i]
+  
+  color_scheme_set("brightblue")
+  # adding title to each plot 
+  x <- stan_hist(stan_toutputs[[i]], pars = c("r", "k", "sigma")) + 
+    ggtitle(paste("Pairs plot:", strain))
+                  
+  
+  print(x)
+}
+
+dev.off()
+
+#### POSTERIOR DISTRIBUTION VISUALIZATION ####
+# plot density --------------------------------------------------------------------
+pdf("03_Output/bayesplot_rk_plots/mcmc_areas.pdf", width = 15, height = 7 , onefile = TRUE)
+
+for (i in 1:length(stan_toutputs)){
+  
+  strain <- names(stan_toutputs)[i]
+  posterior <- as.array(stan_toutputs[[i]])
+  
+
+    p_dens <- mcmc_areas(
+    posterior,
+    pars = c("r", "k",),
+    prob = 0.95,       # 95% int
+    prob_outer = 0.99, # 99% line
+    point_est = "median"
+  ) +
+    ggtitle(paste("Posterior distribution:", names(stan_toutputs)[1])) +
+    theme_minimal()
+  
+  print(p_dens)
+}
+dev.off()
+
+# mcmc_dens -------------------------------------------------------------------
+
+pdf("03_Output/bayesplot_rk_plots/mcmc_dens_rk.pdf", width = 10, height = 5 , onefile = TRUE)
+
+for (i in 1:length(stan_toutputs)){
+  
+  strain <- names(stan_toutputs)[i]
+  f <- as_draws(stan_toutputs[[i]])
+  
+  color_scheme_set("mix-teal-pink")
+  
+  g <- mcmc_dens_overlay(f, pars = c("r", "k", "sigma"),
+                    facet_args = list(nrow = 3)) +
+    facet_text(size = 14) + 
+    ggtitle(paste("Density", names(stan_toutputs)[i])) +
+    theme_minimal()
+  
+  print(g)
+}
+dev.off()
